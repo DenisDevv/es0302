a9799ed82be312cf4c22b126a79b2bb7627ac211
DenisDevv <karajedenis@gmail.com>
2025-02-03 13:00:52 +0100

    inserito IntervalloException

---
+++ src/IntervalloException.java
@@ -0,0 +1,8 @@
+public class IntervalloException extends RuntimeException {
+    public IntervalloException(String message) {
+        super(message);
+    }
+    public IntervalloException() {
+        super("Il numero inserito non è compreso tra 0 e 30!");
+    }
+}
--- src/NumeriPositivi.java
+++ src/NumeriPositivi.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 
 public class NumeriPositivi {
     private ArrayList<Integer> numeriPositivi = new ArrayList<Integer>();
-    public void addNumero(int num) {
+    public void addNumero(int num) throws NegativeException, IntervalloException {
         if (num < 0) {
             throw new NegativeException();
+        }
+        if (num > 30) {
+            throw new IntervalloException();
         }
         numeriPositivi.add(num);
     }
